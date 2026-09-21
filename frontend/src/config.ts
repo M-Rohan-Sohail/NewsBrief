59f4cdcb31efc0a72e5b927380572ced0cff3439
M-Rohan-Sohail <rohansohail196@gmail.com>
2026-09-21 11:33:02 +0500

    Updated Files for Expo Build

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,1 @@
-import { Platform } from 'react-native';
-
-// For Android emulators, localhost points to the emulator itself, not the host machine.
-// 10.0.2.2 is the special alias to your host loopback interface in the Android emulator.
-export const API_URL = Platform.OS === 'android' ? 'http://10.0.2.2:8000' : 'http://127.0.0.1:8000';
+export const API_URL = 'https://brave-brooms-knock.loca.lt';
